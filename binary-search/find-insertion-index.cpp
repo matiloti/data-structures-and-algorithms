52021fad4831f68e654e8be0499fdc88a9df2123
Matias Lotito <149712288+matiloti@users.noreply.github.com>
2026-01-02 15:16:31 +0100

    feat(binary-search): find the insertion index

---
+++ binary-search/find-insertion-index.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int binarySearch(vector<int> v, int target) {
+    int left = 0, right = v.size() - 1;
+    int mid = -1;
+    
+    while(left < right) {
+        mid = floor(left + (right - left) / 2);
+        if(v[mid] == target) return mid;
+        if(target > v[mid]) left = mid + 1;
+        else right = mid;
+    }
+    
+    if(target > v[left]) return right + 1;
+    return left;
+}
+
+int main()
+{
+    vector<int> v1 = {0,1,3,4};
+    std::cout<< binarySearch(v1, 2) << endl; // 2
+    std::cout<< binarySearch(v1, 3) << endl; // 2
+    std::cout<< binarySearch(v1, 0) << endl; // 0
+    std::cout<< binarySearch(v1, 4) << endl; // 3
+    std::cout<< binarySearch(v1, 1) << endl; // 1
+    std::cout<< binarySearch(v1, 5) << endl; // 4
+
+    return 0;
+}
