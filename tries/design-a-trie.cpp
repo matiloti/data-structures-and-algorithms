83bb16442c830174cfe6724f44e7c45251aaac24
Matias Lotito <149712288+matiloti@users.noreply.github.com>
2026-02-13 05:35:00 +0100

    feat: design a trie finished

--- tries/design-a-trie.cpp
+++ tries/design-a-trie.cpp
@@ -6,7 +6,7 @@ using namespace std;
 struct TrieNode {
     char value;
     map<char, TrieNode*> children;
-    boolean isWord;
+    bool isWord;
     TrieNode(char c, bool isWord): value(c), children({}), isWord(isWord) {}
 };
 
@@ -14,35 +14,54 @@ class Trie {
     private:
         TrieNode* root;
     public:
-        TrieNode() {
-            root = TrieNode('-');
+        Trie() {
+            root = new TrieNode('-', false);
         }
         
-        insert(string word) {
+        void insert(string word) {
             TrieNode* curr = root;
             for (int i = 0; i < word.size(); i++) {
                 char c = word[i];
-                bool found = false;
-                for(TrieNode* node : curr->children) {
-                    if(node->value == c) {
-                        curr = node;
-                        found = true;
-                        break;
-                    }
-                }
-                if(!found) {
-                    curr->children.insert(c, new TrieNode(c));
-                }
+                if(curr->children.find(c) == curr->children.end()) 
+                    curr->children.insert({c, new TrieNode(c, false)});
+                curr = curr->children[c];
+                if(i == word.size() - 1) curr->isWord = true;
             }
         }
         
+        bool search(string word) {
+            TrieNode* curr = root;
+            for (int i = 0; i < word.size(); i++) {
+                char c = word[i];
+                if(curr->children.find(c) == curr->children.end()) return false;
+                curr = curr->children[c];
+                if(curr->isWord && i == word.size() - 1) return curr->isWord;
+            }
+            return false;
+        }
+        
+        bool hasPrefix(string prefix) {
+            TrieNode* curr = root;
+            for (int i = 0; i < prefix.size(); i++) {
+                char c = prefix[i];
+                if(curr->children.find(c) == curr->children.end()) return false;
+                curr = curr->children[c];
+            }
+            return true;
+        }
+        
     // TODO
-}
+};
 
 
 int main()
 {
-    // TODO
-
+    Trie trie = Trie();
+    trie.insert("top");
+    trie.insert("bye");
+    cout<<trie.hasPrefix("to");
+    cout<<trie.search("to");
+    trie.insert("to");
+    cout<<trie.search("to");
     return 0;
 }
